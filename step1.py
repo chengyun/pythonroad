2dacd07ed13eb095e2da5574f68a37a7d33d916c
chengyun <vchengyun@163.com>
2018-03-23 19:55:00 +0800

     简单印象，练习输入输出

---
+++ step1.py
@@ -0,0 +1,24 @@
+#简单印象
+
+print("hello, world!")
+
+print('The quick brown fox', 'jumps over', 'the lazy dog')
+
+print(300)
+
+print(100+200)
+
+print('100 + 200 = ',100+200)
+
+"hello"+'world'
+
+100+200
+
+name = input()
+print(name)
+
+name = input("please input your name:")
+print("your name is ",name)
+
+
+print("1024*768=",1024*768)
