597e035a794809738afd375540df798a6d992896
chengyun <vchengyun@163.com>
2018-03-24 18:59:01 +0800

    学习基本数据类型

---
+++ step2.py
@@ -0,0 +1,70 @@
+#基本数据类型
+#int
+123
+-345
+0
+0x11 #16进制
+0b11 #2进制
+
+#float
+3.14
+-1.23
+
+1.2e5
+1.3E5
+-3.4e-4
+
+#string
+"a"
+'abc'
+'I\'m OK!'
+"print('I\\'m OK!')"
+
+print(r"\\") #\\ 运行时不转义 但语法检查时按转义检查
+
+#print(r"\\\") #语法错误
+
+'''abc
+def \n geh
+d'''  #转义
+
+"""abc
+def \n geh
+d"""  #转义
+
+r"""abc
+def \n geh
+d"""  #不转义
+
+#bool
+True
+False
+
+True and False or True and not False
+
+#none
+None
+
+
+
+a = 1
+b = a
+a = a+1
+
+PI = 3
+PI = 3.14 #大写只是常量习惯约定，没有真正限制
+
+3/3 #1.0
+10 / 3 # 3.33333
+10 // 3 #3
+10 % 3 # 1
+
+1 == 1.0 #True
+
+
+
+
+
+
+
+
